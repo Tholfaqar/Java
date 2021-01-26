3381162aa361e80c041fe462f75954d00f67b7b6
Dolf <tmardan@me.berkeley.edu>
2021-01-25 19:05:52 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,13 +5,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
 
-        // int age = scanner.nextInt();
-
-        // System.out.println("Your are is: " + age);
         System.out.println("Principal: ");
-        int principal = scanner.nextInt();
+        int P = scanner.nextInt();
         System.out.println("Interest Rate: ");
         float interest_rate = scanner.nextFloat();
         System.out.println("Period (Years): ");
@@ -20,8 +18,8 @@ public class Main {
         int n = period * 12;
         NumberFormat mortgage = NumberFormat.getCurrencyInstance();
 
-        // P*i/(1-(1+i)^-n)
-        String mortgate_str = mortgage.format(principal * i / (1 - Math.pow((1 + i), -n)));
+        // mortgage = P*i/(1-(1+i)^-n)
+        String mortgate_str = mortgage.format(P * i / (1 - Math.pow((1 + i), -n)));
         System.out.println("Monthly Mortgage Payment: " + mortgate_str);
         scanner.close();
 
