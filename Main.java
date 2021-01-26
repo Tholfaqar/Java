6191387c822ae25042afdddb0a38eb012f2ad60a
Dolf <tmardan@me.berkeley.edu>
2021-01-25 19:14:30 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,20 +5,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        final int MONTHS_IN_YEAR = 12;
+        final int PERCENT = 100;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Principal: ");
         int P = scanner.nextInt();
         System.out.println("Interest Rate: ");
-        float interest_rate = scanner.nextFloat();
+        float yearlyInterest = scanner.nextFloat();
         System.out.println("Period (Years): ");
         int period = scanner.nextInt();
-        float i = interest_rate / 100 / 12;
-        int n = period * 12;
+        float i = yearlyInterest / PERCENT / MONTHS_IN_YEAR; //monthly interest
+        int n = period * MONTHS_IN_YEAR; //loan term in months
         NumberFormat mortgage = NumberFormat.getCurrencyInstance();
 
-        // mortgage = P*i/(1-(1+i)^-n)
+        // P*i/(1-(1+i)^-n)
         String mortgate_str = mortgage.format(P * i / (1 - Math.pow((1 + i), -n)));
         System.out.println("Monthly Mortgage Payment: " + mortgate_str);
         scanner.close();
